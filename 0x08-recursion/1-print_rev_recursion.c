d6b6b4781e75af041eda766956316b664a50a474
d12o6aa <doaakarem798@gmail.com>
2023-04-05 13:45:12 -0700

    7

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * 1-print_rev_recursion.c -o 1-print_rev_recursion
+ * @s: string
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+void _puts_recursion(char *s)
+{
+	if (*s == 0)
+	{
+		_putchar('\n');
+		return;
+	}
+	_puts_recursion(s + 1);
+	_putchar(*s);
+}
+
