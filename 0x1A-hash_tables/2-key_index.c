a775ac53bc6f2d9a1856092ed690d7700552af58
d12o6aa <doaakarem798@gmail.com>
2023-09-08 15:36:46 -0700

    haaa

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -9,7 +9,7 @@
  */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-	unsigned long int i = hash_djb2(*key) % size;
+	unsigned long int i = hash_djb2(key) % size;
 
 	return (i);
 }
