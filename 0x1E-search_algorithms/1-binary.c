3236d30150171b21843a94a18d32cde789958606
d12o6aa <doaakarem798@gmail.com>
2024-01-18 23:13:21 +0200

    update

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -10,7 +10,7 @@
  * Return: the index found or -1
  */
 
-binary_search(int *array, size_t size, int value)
+int binary_search(int *array, size_t size, int value)
 {
 	size_t i = 0;
 	int *a = array;
